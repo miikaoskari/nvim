292678cab16e037d5d45e6d36580a4e6da6899b7
miika <miika.karjalainen13@gmail.com>
2024-03-12 01:07:24 +0200

    add plugins

--- init.lua
+++ init.lua
@@ -36,5 +36,9 @@ require 'cmp-setup'
 -- (file explorer)
 require 'tree'
 
+-- [[ Configure colors ]]
+-- (color)
+require 'color'
+
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
--- lua/options.lua
+++ lua/options.lua
@@ -42,4 +42,7 @@ vim.o.termguicolors = true
 -- set noshowmode
 vim.o.showmode = false
 
+-- disable netrw
+vim.g.nvim_tree_disable_netrw = 0
+
 -- vim: ts=2 sts=2 sw=2 et
