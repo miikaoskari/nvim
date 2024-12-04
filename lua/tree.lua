0f964afd49e19290376be36701b4746a6b84bbf2
miika <miika.karjalainen13@gmail.com>
2024-12-05 00:50:13 +0200

    add more tree keybinds

--- lua/tree.lua
+++ lua/tree.lua
@@ -8,13 +8,19 @@ return {
   },
   cmd = 'Neotree',
   keys = {
-    { 'ee', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
+    { '<leader>ee', ':Neotree toggle<CR>', desc = 'Toggle tr[ee]', silent = true },
+    { '<leader>ef', ':Neotree toggle float<CR>', desc = 'Toggle tr[e]e [f]loat', silent = true },
+    { '<leader>er', ':Neotree toggle right<CR>', desc = 'Toggle tr[e]e [r]ight', silent = true },
+    { '<leader>el', ':Neotree toggle right<CR>', desc = 'Toggle tr[e]e [l]eft', silent = true },
   },
   opts = {
+    use_libuv_file_watcher = true,
+    follow_current_file = {
+      enable = true,
+    },
     filesystem = {
       window = {
         mappings = {
-          ['ee'] = 'close_window',
         },
       },
     },
